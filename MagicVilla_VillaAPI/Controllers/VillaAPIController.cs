409dc1658c30844736003180ff71093445d5734a
Suraj-Rampure <rampuresuraj26@gmail.com>
2025-02-25 13:22:13 +0530

    29. ModelState Validations

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -48,6 +48,12 @@ namespace MagicVilla_VillaAPI.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<VillaDTO> CreateVilla([FromBody]VillaDTO villaDTO)
         {
+            //if(!ModelState.IsValid)
+            //{
+            //    return BadRequest(ModelState);
+            //}
+
+
             if(villaDTO == null)
             {
                 return BadRequest(villaDTO);
