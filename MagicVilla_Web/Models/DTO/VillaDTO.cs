da3a514058c79389073286630eee4644ab9d474a
Suraj-Rampure <rampuresuraj26@gmail.com>
2025-04-03 16:26:28 +0530

    65. Web Project DTO's and API Models

---
+++ MagicVilla_Web/Models/DTO/VillaDTO.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_Web.Models.DTO
+{
+    public class VillaDTO
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        [Required]
+        public int Occupancy { get; set; }
+
+        [Required]
+        public int sqft { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+
+        public string Amenity { get; set; }
+
+        public string Details { get; set; }
+
+        [Required]
+        public double Rate { get; set; }
+
+
+    }
+}
