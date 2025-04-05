e7632a6e0a678442c3a9e27835f4ea99bdfadd12
Suraj-Rampure <rampuresuraj26@gmail.com>
2025-04-06 01:10:40 +0530

    73. Disable Nullable -Important

---
+++ MagicVilla_VillaAPI/Migrations/20250405193254_ChangeNullableToFalse.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagicVilla_VillaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeNullableToFalse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Vilas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Details",
+                table: "Vilas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Amenity",
+                table: "Vilas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SpecialDetails",
+                table: "VilaNumbers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "Vilas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 4, 6, 1, 2, 53, 254, DateTimeKind.Local).AddTicks(9458));
+
+            migrationBuilder.UpdateData(
+                table: "Vilas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2025, 4, 6, 1, 2, 53, 254, DateTimeKind.Local).AddTicks(9471));
+
+            migrationBuilder.UpdateData(
+                table: "Vilas",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2025, 4, 6, 1, 2, 53, 254, DateTimeKind.Local).AddTicks(9473));
+
+            migrationBuilder.UpdateData(
+                table: "Vilas",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2025, 4, 6, 1, 2, 53, 254, DateTimeKind.Local).AddTicks(9474));
+
+            migrationBuilder.UpdateData(
+                table: "Vilas",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2025, 4, 6, 1, 2, 53, 254, DateTimeKind.Local).AddTicks(9476));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Vilas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Details",
+                table: "Vilas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Amenity",
+                table: "Vilas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SpecialDetails",
+                table: "VilaNumbers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Vilas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 3, 13, 1, 16, 15, 724, DateTimeKind.Local).AddTicks(3221));
+
+            migrationBuilder.UpdateData(
+                table: "Vilas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2025, 3, 13, 1, 16, 15, 724, DateTimeKind.Local).AddTicks(3234));
+
+            migrationBuilder.UpdateData(
+                table: "Vilas",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2025, 3, 13, 1, 16, 15, 724, DateTimeKind.Local).AddTicks(3235));
+
+            migrationBuilder.UpdateData(
+                table: "Vilas",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2025, 3, 13, 1, 16, 15, 724, DateTimeKind.Local).AddTicks(3236));
+
+            migrationBuilder.UpdateData(
+                table: "Vilas",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2025, 3, 13, 1, 16, 15, 724, DateTimeKind.Local).AddTicks(3239));
+        }
+    }
+}
