8f0ad62c9ec12332ecfbce1b7e38cecf610216bd
Suraj-Rampure <rampuresuraj26@gmail.com>
2025-04-06 11:30:52 +0530

    74. Update Villa Action Methods

--- MagicVilla_Web/Controllers/VillaController.cs
+++ MagicVilla_Web/Controllers/VillaController.cs
@@ -5,6 +5,7 @@ using MagicVilla_Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace MagicVilla_Web.Controllers
 {
@@ -45,6 +46,36 @@ namespace MagicVilla_Web.Controllers
         public async Task<IActionResult> CreateVilla(VillaCreateDTO model)
         {
            if(ModelState.IsValid)
+            {
+                var response = await _villaService.CreateAsync<APIResponse>(model);
+
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(IndexVilla));
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateVilla(int villaId)
+        {
+            var response = await _villaService.GetAsync<APIResponse>(villaId);
+
+            if (response != null && response.IsSuccess)
+            {
+                VillaDTO model = JsonConvert.DeserializeObject<VillaDTO>(Convert.ToString(response.Result));
+                return View(_mapper.Map<VillaUpdateDTO>(model));
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> UpdateVilla(VillaCreateDTO model)
+        {
+            if (ModelState.IsValid)
             {
                 var response = await _villaService.CreateAsync<APIResponse>(model);
 
