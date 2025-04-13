279789c9792bdc9d1c50a5e56050606cd6bf69d5
Suraj-Rampure <rampuresuraj26@gmail.com>
2025-04-13 12:23:18 +0530

    103. Auth Controller Action Methods 104. Login and Register View

---
+++ MagicVilla_Web/Controllers/AuthController.cs
@@ -0,0 +1,62 @@
+﻿using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.DTO;
+using MagicVilla_Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MagicVilla_Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDTO obj = new();
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginRequestDTO obj)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            RegisterationRequestDTO obj = new();
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterationRequestDTO obj)
+        {
+            APIResponse result = await _authService.RegisterAsync<APIResponse>(obj);
+            if (result != null && result.IsSuccess)
+            {
+                TempData["success"] = "Registration Successful";
+                return RedirectToAction("Login");
+            }
+            return View(); // Return the result to the view for displaying error messages
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            return View();
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
